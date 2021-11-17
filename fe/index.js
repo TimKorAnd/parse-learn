3198c8f7022da1d553211957df9e226fa6562259
TimKorAnd <timkorand@gmail.com>
2021-11-17 10:50:46 +0200

    trying fe

---
+++ fe/index.js
@@ -0,0 +1,6 @@
+// const Parse = require ('../Parse-SDK-JS-3.4.0/index.js');
+import * as Parse from '../Parse-SDK-JS-3.4.0/index.js';
+
+const YOUR_APP_ID = "1234567";
+Parse.initialize(YOUR_APP_ID);
+Parse.serverURL = 'http://localhost:1337/parse'
